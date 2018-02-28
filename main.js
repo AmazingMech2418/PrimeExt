806c44cfefb5743a54883afce65be573177e37b0
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-02-28 17:49:26 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,44 @@
+(function(ext) {
+    // When Scratch closes, do this
+    ext._shutdown = function() {
+        // Nothing here at the moment...
+    };
+ 
+    // Tell Scratch if the extension is ready to do stuff
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'}; // Status 1 = Red, Error // Status 1 = Yellow, Not Ready // Status 2 = Green, Ready
+    };
+ 
+    // Blocks to add to Scratch
+    var descriptor = {
+        blocks: [
+            // Block type, block name, function name
+            ['b', '%n Is Prime?', 3,'prime']
+        ],
+menus: {
+txt: ['hi','bye','how are you?'],
+superMath: ['radian sine', 'radian cosine', 'radian tangent', 'asin', 'atan', 'acos', 'power'],
+geo: ['latitude','longitude']
+}
+    };
+ 
+    // What blocks do go here
+ext.prime = function(a) {
+var rtrn = true;
+if (a>2) {
+for (var i=2; i<a; i++) {
+if (a%i===0) {rtrn=false;}
+}
+} else {
+if (a===2) {
+rtrn = true;
+} else {
+rtrn = false;
+}
+}
+return rtrn;
+}
+    // Name of Scratch Extension goes here
+    ScratchExtensions.register('Custom Reporters', descriptor, ext); 
+     // The name is 'Hello, World!''
+})({});
